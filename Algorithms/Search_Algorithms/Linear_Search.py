557d22c37d38fffba67c8bef6f0b9be5b13d8090
dalinhuang99 <dhuan023@gmail.com>
2019-01-05 09:07:17 -0500

    new sub folder for implemented algorithm (search/sort)

---
+++ Algorithms/Search_Algorithms/Linear_Search.py
@@ -0,0 +1,27 @@
+"""
+    linear search
+
+    Start from the leftmost element of arr[] and one by one compare x with each element of arr[]
+    If x matches with an element, return the index.
+    If x doesn’t match with any of elements, return -1.
+"""
+
+def linearSearch(arr, n, x):
+    for i in range(0, n):
+        if arr[i] == x:
+            return i
+    return -1
+
+str_arr = input('Input an array with number and space (ex: XXX XX XX XXX): ').split(' ')
+int_arr = list(map(int, str_arr))
+target = input('The number you are searching for: ')
+
+print('----------------------------')
+print('Your array will be: ', int_arr)
+print('Your target will be: ', target)
+
+result = linearSearch(int_arr, len(int_arr), target)
+if result == -1:
+    print('The number you are looking for is not in array')
+else:
+    print('Found at index: ', result)
